b977474361801efb09d40879db463de1c2a5c935
Iqbalenter <adek.khanza13@gmail.com>
2026-03-17 08:46:37 +0700

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,20 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 import path from "path";
 import { defineConfig, loadEnv } from "vite";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, ".", "");
   return {
-    plugins: [react(), tailwindcss()],
+    plugins: [react(), tailwindcss(), basicSsl()],
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "."),
       },
     },
     server: {
+      host: true, // Listen on all local IPs
       hmr: process.env.DISABLE_HMR !== "true",
       proxy: {
         // Proxy WebSocket ke backend saat development
